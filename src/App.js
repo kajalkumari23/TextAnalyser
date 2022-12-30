ddd3649ce0944fe8baec0ea057e849a6bd2f2352
Kajal <kajalkumari36240@gmail.com>
2022-12-30 14:19:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+// export default App;
+// import { useState } from 'react';
+import './App.css';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import React, { useState } from 'react';
+// import Alert from './components/Alert';
+ 
+function App() { 
+  const [mode, setMode] = useState('light'); //whether dark mode is enabled or not
+  //const [alert, setAlert] = useState(null)
+
+  // const showAlert = (message, type) =>{
+  //   setAlert({
+  //     msg:message,
+  //     type:type
+  //   })
+  // }
+
+  const toggleMode = ()=>{
+    if(mode ==='light'){
+      setMode ('dark');
+      document.body.style.backgroundColor = 'gray';
+    }else{
+      setMode ('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+    {/* <Navbar/> */}
+    <Navbar title="JoinUs" mode={mode} toggleMode = {toggleMode}/>
+    {/* <Alert alert = "this is alert"/> */}
+    <div className="container my-3">
+    <TextForm heading="Text Analyzer" mode={mode}/>
+    {/* <About/> */}
+   
+    
     </div>
+    </> 
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,104 @@
+import React, {useState} from 'react'
+
+
+export default function TextForm(props) { 
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
+        let newText = text.toUpperCase();
+        setText(newText)  
+    }
+    const handleLowClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText)
+    } 
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value)
+    }
+    const handleClearClick =()=>{
+        let newText = '';
+        setText(newText);
+    }
+    const handleCopy = ()=>{
+        var text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpace = ()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(""));
+    }
+    
+    //   for totitleCase
+      function totitleCase(value) {
+        let myValue = value.toLowerCase()
+        let newValue = myValue.split(" ")
+        let newarray = []
+        for(let i = 0; i< newValue.length; i++){
+            let arrayValue = newValue[i][0].toUpperCase() + newValue[i].slice(1)
+            newarray.push(arrayValue)
+    }
+        return newarray.join(" ")
+    }
+
+    const buttonClick = () => {
+        let newText = totitleCase(text)
+        setText(newText)
+    }
+    const speak = () => {
+        let msg = new SpeechSynthesisUtterance();
+        msg.text = text;
+        window.speechSynthesis.speak(msg);
+      }
+    
+    const handleReverse = (event) => {
+        /* Convert string to array*/
+        let strArr = text.split("");
+        /* Reverse array*/
+        strArr = strArr.reverse();
+        /* Convert array to string*/
+        let newText = strArr.join("");
+        setText(newText);
+      };
+    // to italic
+    const  handleitalic = ()=>{
+        console.log("italic"+ text);
+        var mtext = document.getElementById('myBox');
+       mtext.style.fontStyle= 'italic';
+      }
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className='container' style={{ color:props.mode === 'dark'?'white':'black'}}> 
+            <h1>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor:props.mode === 'dark'?'gray':'white', color:props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button className="btn btn-info mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button className="btn btn-info mx-2"  onClick={handleLowClick}>Convert to Lowercase</button> 
+            <button className="btn btn-info mx-2"  onClick={handleClearClick}>Clear text</button>
+            <button className="btn btn-info mx-2"  onClick={handleCopy}>Copy text</button>
+            <button className="btn btn-info mx-2"  onClick={handleExtraSpace}>Remove space</button>
+            <button className="btn btn-info mx-2" onClick={buttonClick}>Convert to TitleCase</button>
+            <button type="submit" onClick={speak} className="btn btn-info mx-2">Speak</button>
+            <button className="btn btn-info mx-2" onClick={handleReverse}>Reverse</button>
+            <button className="btn btn-info mx-2" onClick={handleitalic}>italic</button>
+            {/* mx-2 is bootstrap class for margin in x */}
+
+        </div>
+        <div className="container my-3 style = {{color:props.mode === 'dark'?'white':'black'}}">
+            <h3>Text summary</h3>
+            <p> Words <b>{text.split(" ").length}</b></p>
+            <p>Characters <b>{text.length}</b></p>
+            <p>Time taken to read  <b>{ 0.008 * text.split(" ").length }</b></p>
+            <h3>Preview</h3>
+            <p>{text.length>0?text:'Enter your text in textbox to preview it here' }</p>
+        </div>
+        </>
+    )
+}
+
